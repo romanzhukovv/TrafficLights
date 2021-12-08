a36c415e23a09e11f24d78cbbca9ecacd142972d
romanzhukovv <zhukov.roman89@gmail.com>
2021-12-08 20:59:43 +0300

    added radius to views

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var trafficLightStakView: UIStackView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        yellowColorView.layer.cornerRadius = yellowColorView.layer.bounds.width/2
+        redColorView.layer.cornerRadius = redColorView.layer.bounds.width/2
+        greenColorView.layer.cornerRadius = greenColorView.layer.bounds.width/2
     }
 
-
+    @IBAction func pressedButton() {
+    }
+    
 }
 
